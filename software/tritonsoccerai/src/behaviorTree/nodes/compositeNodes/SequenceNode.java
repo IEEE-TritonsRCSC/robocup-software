647df82cd99829626d363cacbb07321e98f84eb3
rojiko1 <rkadekar12@outlook.com>
2022-11-19 12:05:26 -0800

    Implemented SequenceNode

--- software/tritonsoccerai/src/behaviorTree/nodes/compositeNodes/SequenceNode.java
+++ software/tritonsoccerai/src/behaviorTree/nodes/compositeNodes/SequenceNode.java
@@ -1,5 +1,28 @@
 package behaviorTree.nodes.compositeNodes;
 
+import behaviorTree.nodes.BTNode;
+
+// executes nodes in sequence until all completed or until one fails
 public class SequenceNode extends CompositeNode {
+
+    private BTNode[] sequence;
+
+    public SequenceNode() {
+        sequence = new BTNode[0];
+    }
+
+    public SequenceNode(BTNode[] sequence) {
+        this.sequence = sequence;
+    }
+
+    public boolean execute() {
+        for (BTNode sequenceNode : this.sequence) {
+            boolean completed = sequenceNode.execute();
+            if (!completed) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
