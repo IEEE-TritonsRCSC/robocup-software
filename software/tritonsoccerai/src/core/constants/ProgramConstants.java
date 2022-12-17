e9037b03e7b81b15e601913adaec1f5795ee7f65
rojiko1 <rkadekar12@outlook.com>
2022-12-17 09:18:28 -0800

    copied constants folder from newer repo

---
+++ software/tritonsoccerai/src/core/constants/ProgramConstants.java
@@ -0,0 +1,15 @@
+package core.constants;
+
+import core.config.*;
+
+public class ProgramConstants {
+
+    public static boolean test = false;
+
+    public static AIConfig aiConfig = null;
+    public static DisplayConfig displayConfig = null;
+    public static GameConfig gameConfig = null;
+    public static NetworkConfig networkConfig = null;
+    public static ObjectConfig objectConfig = null;
+
+}
