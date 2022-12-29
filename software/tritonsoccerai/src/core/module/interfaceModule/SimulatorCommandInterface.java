e54cfab9acffd216d3a52636a72c854ecc724420
rojiko1 <rkadekar12@outlook.com>
2022-12-28 17:00:31 -0800

    Copied interface modules from old repo

---
+++ software/tritonsoccerai/src/core/module/interfaceModule/SimulatorCommandInterface.java
@@ -0,0 +1,168 @@
+package core.module.interfaceModule;
+
+import com.google.protobuf.Any;
+import com.rabbitmq.client.Delivery;
+import core.constants.ProgramConstants;
+import core.fieldObjects.robot.Team;
+import core.module.Module;
+import com.triton.networking.UDP_Client;
+import proto.simulation.SslSimulationConfig;
+import proto.simulation.SslSimulationConfig.SimulatorConfig;
+
+import java.io.IOException;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeoutException;
+
+import static core.messaging.Exchange.AI_SIMULATOR_CONFIG;
+import static core.messaging.Exchange.AI_SIMULATOR_CONTROL;
+import static core.messaging.SimpleSerialize.simpleDeserialize;
+import static proto.simulation.SslGcCommon.RobotId;
+import static proto.simulation.SslGcCommon.Team.BLUE;
+import static proto.simulation.SslGcCommon.Team.YELLOW;
+import static proto.simulation.SslSimulationControl.*;
+import static sslsim.SslSimulationCustomErforceRobotSpec.RobotSpecErForce;
+
+public class SimulatorCommandInterface extends Module {
+    private UDP_Client client;
+    private Future<?> clientFuture;
+
+    public SimulatorCommandInterface(ScheduledThreadPoolExecutor executor) {
+        super(executor);
+    }
+
+    @Override
+    protected void prepare() {
+        try {
+            setupClient();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void declareConsumes() throws IOException, TimeoutException {
+        declareConsume(AI_SIMULATOR_CONTROL, this::callbackSimulatorControl);
+        declareConsume(AI_SIMULATOR_CONFIG, this::callbackSimulatorConfig);
+    }
+
+    private void callbackSimulatorControl(String s, Delivery delivery) {
+        SimulatorControl simulatorControl = (SimulatorControl) simpleDeserialize(delivery.getBody());
+
+        SimulatorCommand.Builder simulatorCommand = SimulatorCommand.newBuilder();
+        simulatorCommand.setControl(simulatorControl);
+        client.addSend(simulatorCommand.build().toByteArray());
+    }
+
+    private void callbackSimulatorConfig(String s, Delivery delivery) {
+        SimulatorConfig simulatorConfig = (SimulatorConfig) simpleDeserialize(delivery.getBody());
+
+        SimulatorCommand.Builder simulatorCommand = SimulatorCommand.newBuilder();
+        simulatorCommand.setConfig(simulatorConfig);
+        client.addSend(simulatorCommand.build().toByteArray());
+    }
+
+    @Override
+    public void interrupt() {
+        super.interrupt();
+        clientFuture.cancel(false);
+    }
+
+    private void setupClient() throws IOException {
+        client = new UDP_Client(ProgramConstants.networkConfig.simulationCommandAddress,
+                ProgramConstants.networkConfig.simulationCommandPort,
+                this::callbackSimulatorResponse,
+                10);
+    }
+
+    private void callbackSimulatorResponse(byte[] bytes) {
+        try {
+            SimulatorResponse simulatorResponse = SimulatorResponse.parseFrom(bytes);
+            System.out.println(simulatorResponse);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        setupSimulator();
+        clientFuture = executor.submit(client);
+    }
+
+    private void setupSimulator() {
+        SimulatorControl.Builder simulatorControl = SimulatorControl.newBuilder();
+
+        for (int id = 6; id < 12; id++) {
+            TeleportRobot.Builder teleportRobot = TeleportRobot.newBuilder();
+            RobotId.Builder robotId = RobotId.newBuilder();
+            robotId.setTeam(BLUE);
+            robotId.setId(id);
+            teleportRobot.setId(robotId);
+            teleportRobot.setPresent(false);
+            simulatorControl.addTeleportRobot(teleportRobot);
+        }
+
+        for (int id = 6; id < 12; id++) {
+            TeleportRobot.Builder teleportRobot = TeleportRobot.newBuilder();
+            RobotId.Builder robotId = RobotId.newBuilder();
+            robotId.setTeam(YELLOW);
+            robotId.setId(id);
+            teleportRobot.setId(robotId);
+            teleportRobot.setPresent(false);
+            simulatorControl.addTeleportRobot(teleportRobot);
+        }
+
+        publish(AI_SIMULATOR_CONTROL, simulatorControl.build());
+
+        SimulatorConfig.Builder simulatorConfig = SimulatorConfig.newBuilder();
+        for (int id = 0; id < 6; id++) {
+            SslSimulationConfig.RobotSpecs.Builder specs = SslSimulationConfig.RobotSpecs.newBuilder();
+            addSpecs(Team.YELLOW, id, specs);
+            addSpecs(Team.BLUE, id, specs);
+            simulatorConfig.addRobotSpecs(specs);
+        }
+
+        publish(AI_SIMULATOR_CONFIG, simulatorConfig.build());
+    }
+
+    private void addSpecs(Team team, int id, SslSimulationConfig.RobotSpecs.Builder specs) {
+        RobotId.Builder robotId = RobotId.newBuilder();
+        if (team == Team.YELLOW)
+            robotId.setTeam(YELLOW);
+        else
+            robotId.setTeam(BLUE);
+        robotId.setId(id);
+
+        specs.setId(robotId);
+        specs.setRadius(ProgramConstants.objectConfig.robotRadius);
+        specs.setHeight(ProgramConstants.objectConfig.robotHeight);
+        specs.setMass(ProgramConstants.objectConfig.robotMass);
+        specs.setMaxLinearKickSpeed(ProgramConstants.objectConfig.robotMaxLinearKickSpeed);
+        specs.setMaxChipKickSpeed(ProgramConstants.objectConfig.robotMaxChipKickSpeed);
+        specs.setCenterToDribbler(ProgramConstants.objectConfig.robotCenterToDribbler);
+
+        SslSimulationConfig.RobotLimits.Builder limits = SslSimulationConfig.RobotLimits.newBuilder();
+        limits.setAccSpeedupAbsoluteMax(ProgramConstants.objectConfig.robotAccSpeedupAbsoluteMax);
+        limits.setAccSpeedupAngularMax(ProgramConstants.objectConfig.robotAccSpeedupAngularMax);
+        limits.setAccBrakeAbsoluteMax(ProgramConstants.objectConfig.robotAccBrakeAbsoluteMax);
+        limits.setAccBrakeAngularMax(ProgramConstants.objectConfig.robotAccBrakeAngularMax);
+        limits.setVelAbsoluteMax(ProgramConstants.objectConfig.robotVelAbsoluteMax);
+        limits.setVelAngularMax(ProgramConstants.objectConfig.robotVelAngularMax);
+        specs.setLimits(limits);
+
+        SslSimulationConfig.RobotWheelAngles.Builder wheelAngles = SslSimulationConfig.RobotWheelAngles.newBuilder();
+        wheelAngles.setFrontRight(ProgramConstants.objectConfig.robotFrontRight);
+        wheelAngles.setBackRight(ProgramConstants.objectConfig.robotBackRight);
+        wheelAngles.setBackLeft(ProgramConstants.objectConfig.robotBackLeft);
+        wheelAngles.setFrontLeft(ProgramConstants.objectConfig.robotFrontLeft);
+        specs.setWheelAngles(wheelAngles);
+
+        RobotSpecErForce.Builder specErForce = RobotSpecErForce.newBuilder();
+        specErForce.setShootRadius(ProgramConstants.objectConfig.shootRadius);
+        specErForce.setDribblerHeight(ProgramConstants.objectConfig.dribblerHeight);
+        specErForce.setDribblerWidth(ProgramConstants.objectConfig.dribblerWidth);
+        specs.addCustom(Any.pack(specErForce.build()));
+    }
+}
---
+++ software/tritonsoccerai/src/core/module/interfaceModule/TritonBotMessageInterface.java
@@ -0,0 +1,108 @@
+package core.module.interfaceModule;
+
+import com.rabbitmq.client.Delivery;
+import core.ai.GameInfo;
+import core.constants.ProgramConstants;
+import core.module.Module;
+import com.triton.networking.UDP_Client;
+import proto.triton.TritonBotCommunication.TritonBotMessage;
+
+import java.io.IOException;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeoutException;
+
+import static core.messaging.Exchange.AI_ROBOT_FEEDBACKS;
+import static core.messaging.Exchange.AI_TRITON_BOT_MESSAGE;
+import static core.messaging.SimpleSerialize.simpleDeserialize;
+import static proto.simulation.SslSimulationRobotFeedback.RobotFeedback;
+
+public class TritonBotMessageInterface extends Module {
+    private Map<Integer, UDP_Client> clientMap;
+    private List<Future<?>> clientFutures;
+    private Map<Integer, RobotFeedback> feedbacks;
+
+    public TritonBotMessageInterface(ScheduledThreadPoolExecutor executor) {
+        super(executor);
+    }
+
+    @Override
+    protected void prepare() {
+        clientMap = new HashMap<>();
+        clientFutures = new ArrayList<>();
+        feedbacks = new HashMap<>();
+
+        try {
+            setupClients();
+        } catch (SocketException | UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void declareConsumes() throws IOException, TimeoutException {
+        declareConsume(AI_TRITON_BOT_MESSAGE, this::callbackTritonBotMessage);
+    }
+
+    private void callbackTritonBotMessage(String s, Delivery delivery) {
+        TritonBotMessage message = (TritonBotMessage) simpleDeserialize(delivery.getBody());
+        if (clientMap.containsKey(message.getId()))
+            clientMap.get(message.getId()).addSend(message.toByteArray());
+    }
+
+    @Override
+    public void interrupt() {
+        super.interrupt();
+        clientFutures.forEach(clientFuture -> clientFuture.cancel(false));
+    }
+
+    private void setupClients() throws SocketException, UnknownHostException {
+        for (int id = 0; id < ProgramConstants.gameConfig.numBots; id++) {
+            String serverAddress;
+            int serverPort;
+            switch (GameInfo.getTeamColor()) {
+                case YELLOW -> {
+                    serverAddress = ProgramConstants.networkConfig.tritonBotAddressYellow;
+                    serverPort = ProgramConstants.networkConfig.tritonBotPortBaseYellow + id * ProgramConstants.networkConfig.tritonBotPortIncr;
+                }
+                case BLUE -> {
+                    serverAddress = ProgramConstants.networkConfig.tritonBotAddressBlue;
+                    serverPort = ProgramConstants.networkConfig.tritonBotPortBaseBlue + id * ProgramConstants.networkConfig.tritonBotPortIncr;
+                }
+                default -> throw new IllegalStateException("Unexpected value: " + GameInfo.getTeamColor());
+            }
+
+            UDP_Client client = new UDP_Client(serverAddress, serverPort, this::callbackTritonBotFeedback, 10);
+            clientMap.put(id, client);
+        }
+    }
+
+    private void callbackTritonBotFeedback(byte[] bytes) {
+        RobotFeedback feedback = null;
+        try {
+            feedback = RobotFeedback.parseFrom(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        feedbacks.put(feedback.getId(), feedback);
+        publish(AI_ROBOT_FEEDBACKS, feedbacks);
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        for (int id = 0; id < ProgramConstants.gameConfig.numBots; id++) {
+            RobotFeedback.Builder feedback = RobotFeedback.newBuilder();
+            feedback.setId(id);
+            feedback.setDribblerBallContact(false);
+            feedbacks.put(id, feedback.build());
+        }
+        clientMap.forEach((id, client) -> clientFutures.add(executor.submit(client)));
+    }
+}
