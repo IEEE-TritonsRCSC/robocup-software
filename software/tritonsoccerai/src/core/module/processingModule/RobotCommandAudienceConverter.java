7bcddd203e1895f358e1fbc93e1ff77034a3a710
jwong1209 <jwong1209qaz@gmail.com>
2023-01-03 19:53:51 -0800

    Added documentation to class

--- software/tritonsoccerai/src/core/module/processingModule/RobotCommandAudienceConverter.java
+++ software/tritonsoccerai/src/core/module/processingModule/RobotCommandAudienceConverter.java
@@ -14,8 +14,14 @@ import static com.triton.messaging.Exchange.AI_ROBOT_COMMAND;
 import static com.triton.messaging.SimpleSerialize.simpleDeserialize;
 import static proto.simulation.SslSimulationRobotControl.*;
 
+/**
+ *  Class for changing a robot biased command to a audience biased command
+ */
 public class RobotCommandAudienceConverter extends Module {
-
+    
+    /*
+     * Constructor
+     */
     public RobotCommandAudienceConverter(ScheduledThreadPoolExecutor executor) {
         super(executor);
     }
@@ -24,23 +30,42 @@ public class RobotCommandAudienceConverter extends Module {
     protected void prepare() {
     }
 
+    /*
+     * Consumes from AI_BIASED_ROBOT_COMMAND and runs callBackBiasedRobotCommand
+     */
     @Override
     protected void declareConsumes() throws IOException, TimeoutException {
         declareConsume(AI_BIASED_ROBOT_COMMAND, this::callbackBiasedRobotCommand);
     }
 
+    /**
+     * Takes a message containing a biased robot command and publishes 
+     * audience biased command to the AI_ROBOT_COMMAND exchange.
+     * @param s - param not used
+     * @param delivery - Message containing biased command for robot
+     */
     private void callbackBiasedRobotCommand(String s, Delivery delivery) {
         RobotCommand biasedRobotCommand = (RobotCommand) simpleDeserialize(delivery.getBody());
         RobotCommand robotCommand = commandBiasedToAudience(biasedRobotCommand);
         publish(AI_ROBOT_COMMAND, robotCommand);
     }
 
+    /**
+     * Takes a robot biased command and builds audience biased command
+     * @param command - robot biased command 
+     * @return - audience biased command
+     */
     private static RobotCommand commandBiasedToAudience(RobotCommand command) {
         RobotCommand.Builder audienceCommand = command.toBuilder();
         audienceCommand.setMoveCommand(moveCommandBiasedToAudience(command.getMoveCommand()));
         return audienceCommand.build();
     }
 
+    /**
+     * Takes a robot biased move command and builds audience biased move command
+     * @param moveCommand - robot biased move command
+     * @return - audience biased move command
+     */
     private static RobotMoveCommand moveCommandBiasedToAudience(RobotMoveCommand moveCommand) {
         if (moveCommand.hasGlobalVelocity()) {
             RobotMoveCommand.Builder audienceMoveCommand = moveCommand.toBuilder();
@@ -51,6 +76,11 @@ public class RobotCommandAudienceConverter extends Module {
         return moveCommand;
     }
 
+    /**
+     * Takes in a robot biased movement velocity and returns audience biased movement velocity
+     * @param globalVelocity - robot biased movement velocity 
+     * @return - audience biased movement velocity 
+     */
     private static MoveGlobalVelocity moveGlobalVelocityBiasedToAudience(MoveGlobalVelocity globalVelocity) {
         MoveGlobalVelocity.Builder audienceGlobalVelocity = globalVelocity.toBuilder();
 
