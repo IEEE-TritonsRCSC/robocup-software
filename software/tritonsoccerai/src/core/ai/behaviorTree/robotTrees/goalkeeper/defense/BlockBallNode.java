58638ef9f7867125f2c2c001b8f23c22c2233c2b
beliang <b3nliang@gmail.com>
2023-01-08 00:02:35 -0800

    Added method to find optimal position on the arc

--- software/tritonsoccerai/src/core/ai/behaviorTree/robotTrees/goalkeeper/defense/BlockBallNode.java
+++ software/tritonsoccerai/src/core/ai/behaviorTree/robotTrees/goalkeeper/defense/BlockBallNode.java
@@ -2,17 +2,38 @@ package core.ai.behaviorTree.robotTrees.goalkeeper.defense;
 
 import core.ai.behaviorTree.nodes.NodeState;
 import core.ai.behaviorTree.nodes.taskNodes.TaskNode;
+import core.fieldObjects.ball.Ball;
+import core.util.Vector2d;
 
 /**
  * Moves goalkeeper to optimal position to block ball
  */
 public class BlockBallNode extends TaskNode {
 
-    // TODO
+    private Vector2d centerArc;
+    private int radius;
+    private Ball ball;
+
+    public BlockBallNode(Ball ball) {
+        super("Block ball node");
+
+        this.centerArc = new Vector2d(0, 0); // arbitrary center arc
+        this.radius = 2;
+        this.ball = ball;
+    }
 
     @Override
     public NodeState execute() {
-        return null;
+        // make it so execute moves robot to the location
+        return NodeState.SUCCESS;
+    }
+
+    public Vector2d findPositioningLocation() {
+        // finds optimal vector on the arc
+        Vector2d optimalLocation = new Vector2d(
+            (this.ball.getX() - this.centerArc.x) * radius, 
+            (this.ball.getY() - this.centerArc.y) * radius);
+        return optimalLocation; 
     }
 
 }
--- software/tritonsoccerai/src/core/ai/behaviorTree/robotTrees/goalkeeper/defense/GKDefenseRootNode.java
+++ software/tritonsoccerai/src/core/ai/behaviorTree/robotTrees/goalkeeper/defense/GKDefenseRootNode.java
@@ -3,6 +3,7 @@ package core.ai.behaviorTree.robotTrees.goalkeeper.defense;
 import core.ai.behaviorTree.nodes.NodeState;
 import core.ai.behaviorTree.nodes.compositeNodes.CompositeNode;
 import core.ai.behaviorTree.nodes.taskNodes.TaskNode;
+import core.fieldObjects.ball.Ball;
 
 /**
  * Defined behavior for goalkeeper when on defense
@@ -12,9 +13,9 @@ public class GKDefenseRootNode extends CompositeNode {
 
     private final TaskNode blockBall;
 
-    public GKDefenseRootNode() {
+    public GKDefenseRootNode(Ball ball) {
         super("GK Defense Node");
-        this.blockBall = new BlockBallNode();
+        this.blockBall = new BlockBallNode(ball);
     }
 
     @Override
