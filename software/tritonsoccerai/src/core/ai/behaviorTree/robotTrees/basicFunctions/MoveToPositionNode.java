9a7b9c86accc5960463900daf987b3ef7f77ddf7
jwong1209 <jwong1209qaz@gmail.com>
2023-01-08 18:59:39 -0800

    worked on MoveToPositionNode

--- software/tritonsoccerai/src/core/ai/behaviorTree/robotTrees/basicFunctions/MoveToPositionNode.java
+++ software/tritonsoccerai/src/core/ai/behaviorTree/robotTrees/basicFunctions/MoveToPositionNode.java
@@ -1,13 +1,19 @@
 package core.ai.behaviorTree.robotTrees.basicFunctions;
 
+import java.util.LinkedList;
+
+import core.ai.GameInfo;
 import core.ai.behaviorTree.nodes.NodeState;
 import core.ai.behaviorTree.nodes.taskNodes.TaskNode;
 import core.fieldObjects.robot.Ally;
 import core.util.Vector2d;
+import core.search.implementation.*;
+import core.search.node2d.Node2d;
 
 public class MoveToPositionNode extends TaskNode {
 
-
+    PathfindGridGroup pathfindGridGroup;
+    
     public MoveToPositionNode(Ally ally) {
         super("Move To Position Node: " + ally.toString(), ally);
     }
@@ -18,9 +24,16 @@ public class MoveToPositionNode extends TaskNode {
     }
 
     public NodeState execute(Vector2d endLoc) {
-        // TODO
-        // use pathfinding module to find direction to move
-        // send command to move in that direction
+
+        Vector2d allyPos = super.ally.getPos();
+
+        //TODO: Can't get ally ID, no ID field
+        LinkedList<Node2d> route = pathfindGridGroup.findRoute(ally.getId(), allyPos, endLoc);
+        Vector2d next = pathfindGridGroup.findNext(actor.getId(), route);
+
+        //TODO: Need to publish command for movement
+        
+
         return NodeState.SUCCESS;
     }
 
