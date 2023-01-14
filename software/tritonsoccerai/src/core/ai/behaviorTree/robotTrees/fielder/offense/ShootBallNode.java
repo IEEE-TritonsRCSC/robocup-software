3bceffb06e25504150dcfbb769f3e1089d727c7e
[Kazuma] <[kazuma0612touhoku@gmail.com]>
2023-01-15 07:29:07 +0900

    almost fully implemented ShootBallNode

--- software/tritonsoccerai/src/core/ai/behaviorTree/robotTrees/fielder/offense/ShootBallNode.java
+++ software/tritonsoccerai/src/core/ai/behaviorTree/robotTrees/fielder/offense/ShootBallNode.java
@@ -1,14 +1,28 @@
 package core.ai.behaviorTree.robotTrees.fielder.offense;
 
+import core.ai.GameInfo;
 import core.ai.behaviorTree.nodes.NodeState;
 import core.ai.behaviorTree.nodes.compositeNodes.SequenceNode;
 import core.ai.behaviorTree.robotTrees.basicFunctions.KickBallNode;
 import core.constants.RobotConstants;
 import core.fieldObjects.robot.Ally;
+import core.fieldObjects.robot.Foe;
+
 import core.util.Vector2d;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static core.constants.ProgramConstants.aiConfig;
+
+//import protofiles
+import static proto.triton.FilteredObject.FilteredWrapperPacket;
+import static proto.triton.FilteredObject.Robot;
+import static proto.vision.MessagesRobocupSslGeometry.SSL_GeometryFieldSize;
+
 /**
- * Shoots ball in the calculated optimal direction at max velocity
+ * Shoots ball in the calculated optimal direction at max velocity.
+ * It is assumed that the ally kicks the ball from the spot when he has it.
  */
 public class ShootBallNode extends SequenceNode {
 
@@ -23,7 +37,7 @@ public class ShootBallNode extends SequenceNode {
      * Kicks ball as fast as possible in the optimal direction
      */
     public NodeState execute() {
-        // TODO
+        // kicks the ball
         this.kickBall.execute(findShot(), RobotConstants.MAX_KICK_VELOCITY, false);
         return NodeState.SUCCESS;
     }
@@ -32,8 +46,48 @@ public class ShootBallNode extends SequenceNode {
      * Finds the direction of the optimal shot
      */
     private Vector2d findShot() {
-        // TODO
-        return null;
+
+        // TODO Get the field parameter
+        SSL_GeometryFieldSize field = wrapper.getField();
+
+        ArrayList<Foe> foesList = new ArrayList<>(GameInfo.getFoeFielders());
+        ArrayList<Ally> allysList = new ArrayList<>(GameInfo.getFielders());
+        List<Robot> obstacles = new ArrayList<>();
+
+        //remove the ally closest to ball
+        allysList.remove(GameInfo.getAllyClosestToBall());
+
+        //add the other allys and foes to the obstacles list
+        obstacles.add(allysList);
+        obstacles.addAll(foesList);
+
+        //get the goal parameters
+        float goalMinX = -field.getGoalWidth() / 2f + 300f;
+        float goalMaxX = field.getGoalWidth() / 2f - 300f;
+        float goalY = field.getFieldLength() / 2f;
+
+        
+        List<Vector2d> kickTos = new ArrayList<>();
+        for (float goalX = goalMinX; goalX < goalMaxX; goalX += aiConfig.goalShootKickToSearchSpacing)
+            kickTos.add(new Vector2d(goalX, goalY));
+
+        //best kick direction
+        Vector2d bestKickTo = null;
+        float maxScore = -Float.MAX_VALUE;
+
+
+        //defines the best kick direction based on the position of the obstacles
+        for (Vector2d kickTo : kickTos) {
+            float distToObstacles = distToPath(GameInfo.getAllyClosestToBall().getPos(), kickTo, obstacles);
+            float score = aiConfig.goalShootDistToObstaclesScoreFactor * distToObstacles;
+
+            if (score > maxScore) {
+                bestKickTo = kickTo;
+                maxScore = score;
+            }
+        }
+
+        return bestKickTo;
     }
 
 }
