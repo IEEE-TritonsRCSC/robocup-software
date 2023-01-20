beff58d74a53c84cb027739b2909fa7f72d41a37
jwong1209 <jwong1209qaz@gmail.com>
2023-01-20 14:35:51 -0800

    Implemented CatchBallNode

--- software/tritonsoccerai/src/core/ai/behaviorTree/robotTrees/basicFunctions/CatchBallNode.java
+++ software/tritonsoccerai/src/core/ai/behaviorTree/robotTrees/basicFunctions/CatchBallNode.java
@@ -3,6 +3,8 @@ package core.ai.behaviorTree.robotTrees.basicFunctions;
 import core.ai.behaviorTree.nodes.NodeState;
 import core.ai.behaviorTree.nodes.taskNodes.TaskNode;
 import core.fieldObjects.robot.Ally;
+import core.fieldObjects.ball;
+import core.ai.GameInfo;
 
 /**
  * Defines task of catching ball in motion
@@ -15,7 +17,22 @@ public class CatchBallNode extends TaskNode {
 
     @Override
     public NodeState execute() {
-        // TODO
+        // Calculating path from robot to ball
+        Ball ball = GameInfo.getBall();
+        Vector2d ballPos = getPos(ball);
+        Vector2d ballToActor = getPos(ally).sub(ballPos);
+        Vector2d offset = ballToActor.project(ball.getVel());
+        Vector2d targetPos = ballPos.add(offset);
+
+
+        // Move to ball
+        MoveToPositionNode MoveToPosition = new MoveToPositionNode(ally);
+        MoveToPosition.execute(targetPos);
+
+        // Dribble It
+        Dribble dribbleBall = new Dribble(ally);
+        dribbleBall.execute();
+
         return NodeState.SUCCESS;
     }
 
