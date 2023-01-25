16100ad4a8e0e902637e1440f1a3cfdea0c74687
beliang <b3nliang@gmail.com>
2023-01-24 21:51:12 -0800

    Naive stop state function, move to its own location

--- software/tritonsoccerai/src/core/ai/behaviorTree/robotTrees/fielder/specificStateFunctions/HaltNode.java
+++ software/tritonsoccerai/src/core/ai/behaviorTree/robotTrees/fielder/specificStateFunctions/HaltNode.java
@@ -2,7 +2,9 @@ package core.ai.behaviorTree.robotTrees.fielder.specificStateFunctions;
 
 import core.ai.behaviorTree.nodes.NodeState;
 import core.ai.behaviorTree.nodes.compositeNodes.SequenceNode;
+import core.ai.behaviorTree.robotTrees.basicFunctions.MoveToPositionNode;
 import core.fieldObjects.robot.Ally;
+import core.util.Vector2d;
 
 /**
  * Handles Halt game state
@@ -11,15 +13,18 @@ import core.fieldObjects.robot.Ally;
 public class HaltNode extends SequenceNode {
 
     private final Ally ally;
+    private final MoveToPositionNode moveToPositionNode;
 
     public HaltNode(Ally ally) {
         super("Halt Node: " + ally.toString());
         this.ally = ally;
+        this.moveToPositionNode = new MoveToPositionNode(ally);
     }
 
     @Override
     public NodeState execute() {
         // TODO stop the robot's motion
+        this.moveToPositionNode.execute(new Vector2d(ally.getX(), ally.getY()));
         return NodeState.SUCCESS;
     }
 
--- software/tritonsoccerai/src/core/ai/behaviorTree/robotTrees/goalkeeper/specificStateFunctions/GKHaltNode.java
+++ software/tritonsoccerai/src/core/ai/behaviorTree/robotTrees/goalkeeper/specificStateFunctions/GKHaltNode.java
@@ -1,19 +1,29 @@
 package core.ai.behaviorTree.robotTrees.goalkeeper.specificStateFunctions;
 
+import java.util.Vector;
+
 import core.ai.GameInfo;
 import core.ai.behaviorTree.nodes.NodeState;
 import core.ai.behaviorTree.nodes.taskNodes.TaskNode;
+import core.ai.behaviorTree.robotTrees.basicFunctions.MoveToPositionNode;
+import core.util.Vector2d;
 
 public class GKHaltNode extends TaskNode {
 
+    private final MoveToPositionNode moveToPositionNode;
+
     public GKHaltNode() {
         super("GK Halt Node: " + GameInfo.getKeeper().toString(), GameInfo.getKeeper());
+        moveToPositionNode = new MoveToPositionNode(GameInfo.getKeeper());
     }
 
     @Override
     public NodeState execute() {
         // TODO send a command to stop the robot
         // No need to update the velocity
+        // move to current location.
+        this.moveToPositionNode.execute(new Vector2d(GameInfo.getKeeper().getX(), 
+                                                    GameInfo.getKeeper().getY()));
         return NodeState.SUCCESS;
     }
 
