cb0ff75dbf28b54965f71366598eba0e71efc96c
TylerFlar <tulikkovulpus@gmail.com>
2023-01-26 18:03:34 -0800

    Implement the vision filter and global to local

--- software/tritonbot/python/module/interface_module/simulator_robot_control_interface.py
+++ software/tritonbot/python/module/interface_module/simulator_robot_control_interface.py
@@ -20,4 +20,45 @@ class SimulatorRobotControlInterface(Module):
 
     def load_config(self):
         super().load_config()
-        self.network_config
+        self.network_config = read_config(
+            ConfigPath.NETWORK_CONFIG)
+
+    def prepare(self):
+        super().prepare()
+        self.setup_client()
+
+    def run(self):
+        super().run()
+        self.consume()
+    
+    def setup_client(self):
+        if (RuntimeConstants.team == Team.YELLOW):
+            ally_control_address = self.network_config['simulationRobotControlAddressYellow']
+            ally_control_port = self.network_config['simulationRobotControlPortYellow']
+        else:
+            ally_control_address = self.network_config['simulationRobotControlAddressBlue']
+            ally_control_port = self.network_config['simulationRobotControlPortBlue']
+        
+        self.client = UDP_Client(
+            server_address=ally_control_address,
+            server_port=ally_control_port,
+            callback=self.callback_robot_control_response
+        )
+
+        self.client.start()
+    
+    def callback_local_command(self, ch, method, properties, body):
+        local_command = RobotCommand()
+        local_command.ParseFromString(body)
+        print(local_command)
+        robot_control = RobotControl()
+        robot_control.robot_commands.append(local_command)
+        self.client.add_send(robot_control.SerializeToString())
+
+    def callback_robot_control_response(self, bytes):
+        response = RobotControlResponse()
+        response.ParseFromString(bytes)
+        for feedback in response.feedback:
+            if (feedback.id != RuntimeConstants.id):
+                continue
+            self.publish(exchange=Exchange.TB_FEEDBACK, object=feedback)
