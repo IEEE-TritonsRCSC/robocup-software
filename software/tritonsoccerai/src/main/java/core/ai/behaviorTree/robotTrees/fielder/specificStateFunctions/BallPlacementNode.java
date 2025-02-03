4ed6292aaf921471dbac2bb8220e08863428426e
YashTandon05 <yashtandon2005@gmail.com>
2025-02-02 20:24:37 -0800

    Implemented smoother and reliable Ball Placement

--- software/tritonsoccerai/src/main/java/core/ai/behaviorTree/robotTrees/fielder/specificStateFunctions/BallPlacementNode.java
+++ software/tritonsoccerai/src/main/java/core/ai/behaviorTree/robotTrees/fielder/specificStateFunctions/BallPlacementNode.java
@@ -13,6 +13,15 @@ import static core.util.ProtobufUtils.getPos;
 import static core.util.ObjectHelper.awardedBall;
 import core.util.Vector2d;
 
+import core.constants.ProgramConstants;
+import static core.constants.RobotConstants.*;
+import java.io.IOException;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Future;
+
+
 /**
  * Handles Ball Placement game state
  * If our possession, place ball at designated location
@@ -23,13 +32,16 @@ public class BallPlacementNode extends TaskNode {
     private final DribbleBallNode dribbleBallNode;
     private final MoveToPositionNode moveToPositionNode;
     private final ChaseBallNode chaseBallNode;
+    private boolean positioned;
 
     public BallPlacementNode(int allyID, ClosestToBallNode closestToBallNode) {
         super("Ball Placement Node: " + allyID, allyID);
         this.closestToBallNode = closestToBallNode;
         this.dribbleBallNode = new DribbleBallNode(allyID);
         this.moveToPositionNode = new MoveToPositionNode(allyID);
+        this.moveToPositionNode.setAvoidBall(true);
         this.chaseBallNode = new ChaseBallNode(allyID);
+        this.positioned = false;
     }
 
     /**
@@ -38,26 +50,65 @@ public class BallPlacementNode extends TaskNode {
      */
     @Override
     public NodeState execute() {
-        float DISTANCE_CONSTANT = 100;
+        Vector2d finalPos = GameInfo.getBallPlacementLocation();
+
+        // TO DO : Need to add condition for it to stop dribbling. 
+
         if (awardedBall() && (closestToBallNode.execute() == NodeState.SUCCESS)) {
+            float DISTANCE_CONSTANT = 1000;
             if (!GameInfo.getPossessBall(allyID)) {
-                this.chaseBallNode.execute();
+                Vector2d ballPos = new Vector2d(GameInfo.getBall().getX(),GameInfo.getBall().getY());
+                Vector2d robotPos = getPos(GameInfo.getAlly(allyID));
+
+                Vector2d moveDirection = new Vector2d(
+                    finalPos.x - ballPos.x,
+                    finalPos.y - ballPos.y
+                );
+                
+                Vector2d moveDirectionUnit = new Vector2d(
+                    moveDirection.x / moveDirection.mag(),
+                    moveDirection.y / moveDirection.mag()
+                );
+                
+                Vector2d initRobotPos = new Vector2d(
+                    ballPos.x - moveDirectionUnit.x * DISTANCE_CONSTANT,
+                    ballPos.y - moveDirectionUnit.y * DISTANCE_CONSTANT
+                );
+
+                float distanceToInit = robotPos.dist(initRobotPos);
+                
+                if (distanceToInit <= 100) {
+                    this.positioned = true;
+                }
+
+                if (!this.positioned) {
+                    this.moveToPositionNode.execute(initRobotPos);
+                }
+                else {
+                    this.moveToPositionNode.setAvoidBall(false);
+                    this.chaseBallNode.execute();
+                    this.dribbleBallNode.execute(finalPos);
+                }
+                
             }
             else {
-                this.dribbleBallNode.execute(GameInfo.getBallPlacementLocation());
+                this.chaseBallNode.execute();
+                this.dribbleBallNode.execute(finalPos);
+                try {
+                    TimeUnit.MILLISECONDS.sleep(ProgramConstants.LOOP_DELAY);
+                } catch (Exception e) {}
             }
         }
         else {
             // move away from placement location if close to it
             Robot ally = GameInfo.getAlly(allyID);
             Vector2d allyPos = getPos(ally);
-            Vector2d ballPos = GameInfo.getBallPlacementLocation();
             // System.out.println("ballposition:" + ballPos);
             // System.out.println("allyposition:" + allyPos);
-            Vector2d ballToAlly = getPos(ally).sub(ballPos);
+            Vector2d ballToAlly = getPos(ally).sub(finalPos);
             // System.out.println("Dist:" + ballToAlly);
             if (ballToAlly.mag() < 2000) {
-                Vector2d targetPos =  ballPos.add(new Vector2d(10 * (allyPos.x - ballPos.x), 10 * (allyPos.y - ballPos.y)));
+                Vector2d targetPos =  finalPos.add(new Vector2d(10 * (allyPos.x - finalPos.x), 10 * (allyPos.y - finalPos.y)));
                 // System.out.println("targetPos: " + targetPos);
                 // MoveToPositionNode MoveToPosition = new MoveToPositionNode(allyID);
                 // MoveToPosition.execute(targetPos);
